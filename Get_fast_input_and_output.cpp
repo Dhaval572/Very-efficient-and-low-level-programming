1b793eed121dd01c66a7003258aeaec9a736a2a8
Dhaval <dp9718776@gmail.com>
2025-03-16 23:25:16 +0530

    Create Get_fast_input_and_output.cpp

---
+++ Get_fast_input_and_output.cpp
@@ -0,0 +1,14 @@
+// Get ultra fast input and output
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); // Disable synchronization
+	cin.tie(NULL);	// Fast input
+	cout.tie(NULL); // Fast output
+
+	int n;
+	cin >> n;
+	cout << n << "\n";
+}
