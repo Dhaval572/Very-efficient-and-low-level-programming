26c9b619b0367b6d634c33c350709d2a3bb0472f
Dhaval <dp9718776@gmail.com>
2025-03-29 22:33:23 +0530

    Create Auto_memory_manager_array.cpp

---
+++ Auto_memory_manager_array.cpp
@@ -0,0 +1,50 @@
+// Auto array memory manager
+// ( note: Memory freed automatically when 'numbers' goes out of scope )
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class AutoArray
+{
+private:
+    T *ptr = nullptr;
+    size_t size = 0;
+
+public:
+    // Constructor - allocates memory
+    AutoArray(size_t array_size) : size(array_size)
+    {
+        ptr = new T[size];
+        cout << "Allocated " << size << " elements\n";
+    }
+
+    // Destructor - frees memory
+    ~AutoArray()
+    {
+        delete[] ptr;
+        cout << "Freed memory\n";
+    }
+
+    // Prevent copying
+    AutoArray(const AutoArray &) = delete;
+    AutoArray &operator=(const AutoArray &) = delete;
+
+    // Simple array access
+    T &operator[](size_t index)
+    {
+        return ptr[index];
+    }
+
+    size_t getSize() const { return size; }
+};
+
+int main()
+{
+    AutoArray<int> numbers(3);
+
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+    
+    return 0;
+}
